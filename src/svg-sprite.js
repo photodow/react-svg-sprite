5e99338e13a83168b2bd47bd0548bde0cdeeb93f
James Dow <james.dow@us.ibm.com>
2016-06-08 22:11:14 -0500

    finished svg-sprite component

---
+++ src/svg-sprite.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Symbol from './symbol';
+
+export default class SvgSprite extends React.Component {
+    render () {
+        const {symbols, style, ...other} = this.props;
+
+        return (
+            <svg {...other} style={style} xmlns="http://www.w3.org/2000/svg">
+                {symbols.map(symbol => {
+                    return <Symbol symbol={symbol} />;
+                })}
+            </svg>
+        );;
+    }
+}
+
+SvgSprite.propTypes = {
+    symbols: React.PropTypes.arrayOf(
+        React.PropTypes.object.isRequired
+    ).isRequired,
+    style: React.PropTypes.object
+};
+
+SvgSprite.defaultProps = {
+    style: {display: 'none'}
+};
